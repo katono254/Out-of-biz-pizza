f107e499abde71b725ae2b9bd5351325ca717440
jakesIII <jayruma@yahoo.com>
2019-10-06 19:05:34 +0300

    Add java script to the section of thpes in the index.html

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,66 @@ $("#place-order").click(function(){
     let crust = $("#crust option:selected").val();
     let topping = $("#toppings option:selected").val();
     let number = $("#number").val();
+    let order = (f, s, c, t, n, total) => {
+        return {f, s, c, t, n, total};
+    };
+    let price, totalPrice
+    switch (type) {
+        case type="matumbo":
+        switch (size){
+            case size="small"
+            price=100;
+            if (crust==="thin"){
+            totalPrice = (price * number) + 50;
+            } else if (crust === "thick") {
+            totalPrice = (price * number) + 70;
+            } else if(crust ==="big"){
+            totalPrice = (price * number) + 200;
+            } else {
+            totalPrice = (price * number) + 190;
+            }
+            break;
+            case size = "medium":
+            price = 200;
+           if (crust === "thin") {
+            totalPrice = (price * number) + 50;
+            } else if (crust === "thick") {
+            totalPrice = (price * number) + 70;
+            } else if (crust === "big") {
+            totalPrice = (price * number) + 200;
+            } else {
+            totalPrice = (price * number) + 190;
+            }
+            break;
+            case size = "large":
+            price = 400;
+            if (crust === "thin") {
+            totalPrice = (price * number) + 50;
+            } else if (crust === "thick") {
+            totalPrice = (price * number) + 70;
+            } else if (crust === "big") {
+            totalPrice = (price * number) + 200;
+            } else {
+            totalPrice = (price * number) + 190;
+            }
+            break;
+        }
+         break;
+         case type = "matumbo":
+                switch (size) {
+                    case size = "small":
+                        price = ;
+                        if (crust === "thin") {
+                            totalPrice = (price * number) + 50;
+                        } else if (crust === "thick") {
+                            totalPrice = (price * number) + 70;
+                        } else if (crust === "big") {
+                            totalPrice = (price * number) + 200;
+                        } else {
+                            totalPrice = (price * number) + 190;
+                        }
+                        break;   
+          
+    }
 
 })
